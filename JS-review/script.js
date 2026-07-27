3776a5ef7444e270cb2532a390868103731a0f2e
Altana <altn.grishkina@capgemini.com>
2026-07-28 00:39:35 +0200

    24. Optional chaining

--- JS-review/script.js
+++ JS-review/script.js
@@ -146,7 +146,7 @@ function getBook(id) {
 // 18. Destructuring objects and arrays 
 const books = getBooks();
 
-const book = getBook(2);
+const book = getBook(3);
 
 // const title = book.title;
 // const author = book.author;
@@ -222,8 +222,23 @@ console.log(false || 'some string');
 console.log(book.translations.spanish);
 
 const spanishTranslation = book.translations.spanish || 'Not translated';
-spanishTranslation
+spanishTranslation;
 
-console.log(book.reviews.librarything.reviewsCount);
+/* console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || "no data";
+countWrong;
 
+const count = book.reviews.librarything.reviewsCount ??  "no data"
+count;
+ */
+// 24. Optional Chaining
 
+function getTotalReviewCount(book){
+  const goodreads = book.reviews?.goodreads?.reviewsCount;
+  goodreads
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book));
+ 
